799694a46b4ef3e05b0398ed79227292670d00a4
Mark <mark.fabon29@gmail.com>
2018-08-21 14:54:31 +0800

    AdminUpdate start on click changed to FacultyViewSubject List adapter for array in the Lisview FacultyViewSubject customlist.xml for custom style of row

--- app/src/main/java/plm/oop/com/plmac/FacultyViewSubject.java
+++ app/src/main/java/plm/oop/com/plmac/FacultyViewSubject.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,56 +29,62 @@ public class FacultyViewSubject extends AppCompatActivity {
     ListView listView;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_view_subject);
+        Intent i = getIntent();
+        final String userName = i.getStringExtra("userName");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference mRef = database.getReference("Subject");
 
 
-            setContentView(R.layout.activity_faculty_view_subject);
-            Intent i= getIntent();
-            final String userName= i.getStringExtra("userName");
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference mRef = database.getReference("Subject");
+        Query zonesQuery = mRef.orderByChild("Faculty").equalTo(userName);
+        zonesQuery.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<String> subject = new ArrayList<>();
+                ArrayList<String> room = new ArrayList<>();
+                ArrayList<String> schedule = new ArrayList<>();
+                for (DataSnapshot zoneSnapshot : dataSnapshot.getChildren()) {
+                    subject.add(zoneSnapshot.child("Name").getValue(String.class));
+                    room.add(zoneSnapshot.child("Room").getValue(String.class));
+                    schedule.add(zoneSnapshot.child("Schedule").getValue(String.class));
+                }
 
 
-            Query zonesQuery = mRef.orderByChild("Faculty").equalTo(userName);
-            zonesQuery.addValueEventListener(new ValueEventListener() {
+                String[] subjectArr = subject.toArray(new String[0]);
+                String[] roomArr = room.toArray(new String[0]);
+                String[] scheduleArr = schedule.toArray(new String[0]);
 
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    ArrayList<String> subject = new ArrayList<>();
-                    ArrayList<String> room = new ArrayList<>();
-                    ArrayList<String> schedule = new ArrayList<>();
-                    for (DataSnapshot zoneSnapshot : dataSnapshot.getChildren()) {
-                        subject.add(zoneSnapshot.child("Name").getValue(String.class));
-                        room.add(zoneSnapshot.child("Room").getValue(String.class));
-                        schedule.add(zoneSnapshot.child("Schedule").getValue(String.class));
+                ListAdapter place = new ListAdapter(FacultyViewSubject.this, subjectArr, roomArr, scheduleArr);
+
+                listView = findViewById(R.id.vslv1);
+                listView.setAdapter(place);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent (FacultyViewSubject.this,ClickedOnList.class );
+                        intent.putExtra("Prof", userName);
+                        startActivity(intent);
                     }
+                });
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-                    String[] subjectArr = subject.toArray(new String[0]);
-                    String[] roomArr = room.toArray(new String[0]);
-                    String[] scheduleArr = schedule.toArray(new String[0]);
+            }
 
-                    ListAdapter place = new ListAdapter(FacultyViewSubject.this, subjectArr, roomArr, scheduleArr);
-
-                    listView = findViewById(R.id.vslv1);
-                    listView.setAdapter(place);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-
-
-        }
+        });
 
 
     }
+
+
+}
 
 
