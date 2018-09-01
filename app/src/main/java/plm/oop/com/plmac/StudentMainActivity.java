5543590bd8a80b0a5ee11d16c32aaee6e17b46c2
Peter Portes <peteramiel@gmail.com>
2018-09-01 19:57:13 +0800

    Menu

--- app/src/main/java/plm/oop/com/plmac/StudentMainActivity.java
+++ app/src/main/java/plm/oop/com/plmac/StudentMainActivity.java
@@ -99,15 +99,8 @@ public class StudentMainActivity extends AppCompatActivity implements Navigation
                     tvStudentViewProfileStudentNumber.setText("Student Number: " + student.child("userNumber").getValue(String.class));
                 }
             }
-
-            @Override
-<<<<<<< Updated upstream
-            public void onClick(View view) {
-                startActivity(new Intent(StudentMainActivity.this,NewsStudentActivity.class));
-=======
             public void onCancelled(DatabaseError databaseError) {
 
->>>>>>> Stashed changes
             }
         });
         tvStudentViewProfileProgram = findViewById(R.id.tvStudentViewProfileProgram);
